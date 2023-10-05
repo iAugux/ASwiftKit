36e0450f6a29a763c887427cb81b250b32ca5cd5
iAugus <iAugux@gmail.com>
2023-10-05 11:32:57 +0800

    update

--- Sources/ASwiftKit/Extensions/UIApplication+.swift
+++ Sources/ASwiftKit/Extensions/UIApplication+.swift
@@ -24,4 +24,39 @@ public extension UIApplication {
             .first
     }
 }
+
+extension UIViewController {
+    public static var current: UIViewController {
+        var scenes = UIApplication.shared.connectedScenes.filter { $0.activationState == .foregroundActive }
+        if scenes.isEmpty {
+            // If it's called just after app launched, there may be no active scenes at all.
+            scenes = UIApplication.shared.connectedScenes.filter { $0.activationState == .foregroundInactive }
+        }
+        guard let scene = (scenes.compactMap { $0 as? UIWindowScene }).first else {
+            return UIViewController()
+        }
+        guard let vc = scene.windows.first(where: { $0.isKeyWindow })?.rootViewController else {
+            return UIViewController()
+        }
+        return vc.findBestViewController()
+    }
+
+    private func findBestViewController() -> UIViewController {
+        if let vc = presentedViewController {
+            // returns presented view controller
+            return vc
+        } else if let vc = self as? UISplitViewController {
+            // returns right hand side
+            return vc.viewControllers.last ?? vc
+        } else if let vc = self as? UINavigationController {
+            // returns top view controller
+            return vc.topViewController ?? vc
+        } else if let vc = self as? UITabBarController {
+            // returns visible view
+            return vc.selectedViewController ?? vc
+        } else {
+            return self
+        }
+    }
+}
 #endif
